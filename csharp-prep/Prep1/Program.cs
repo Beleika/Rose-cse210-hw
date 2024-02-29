e00e7040aaa17d82e4006e7b4ca3b8cd254559e8
Beleika <sylvestreberlin@gmail.com>
2024-02-29 11:20:58 -0500

    add

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+
+        Console.Write("What is your first Name ? ");
+        string first =  Console.ReadLine();
+
+        Console.Write("What is your last Name ? ");
+        string last =  Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+
+        
+       
     }
 }
